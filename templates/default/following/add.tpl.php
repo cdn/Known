5c2ba22d26ff6839758aa6f9a654183752a1b079
C <59314073+definitelycn@users.noreply.github.com>
2020-01-29 12:18:40 +0000

    Allow for localisation of button text "Add"

--- templates/default/following/add.tpl.php
+++ templates/default/following/add.tpl.php
@@ -2,7 +2,7 @@
     <p>
         <?php echo \Idno\Core\Idno::site()->language()->_('Add the address of a site to follow'); ?>:<br>
             <input type="text" name="url" value="" placeholder="https:// ..." style="width: 100%"><br>
-        <input type="submit" class="btn btn-primary" value="Add" style="width: 15%">
+        <input type="submit" class="btn btn-primary" value="<?php echo \Idno\Core\Idno::site()->language()->_('Add') ?>" style="width: 15%">
         <?php echo \Idno\Core\Idno::site()->actions()->signForm('following/add')?>
     </p>
 </form>
