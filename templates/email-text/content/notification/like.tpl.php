13f87c29332b34184dcaf29e665cda385bfac536
C <cdn+github@chrs.nl>
2020-02-01 17:35:35 +0000

    Localise like notification emails

--- templates/email-text/content/notification/like.tpl.php
+++ templates/email-text/content/notification/like.tpl.php
@@ -3,8 +3,10 @@ $notification = $vars['notification'];
 $annotation   = $notification->getObject();
 $post         = $notification->getTarget();
 ?>
+<?php echo \Idno\Core\Idno::site()->language()->_('Hi! We wanted to let you know that *%s* liked the post *%s*', [$annotation['owner_name'], $post->getNotificationTitle()]) ?><br>
 Hi! We wanted to let you know that *<?php echo $annotation['owner_name']?>* liked the post *<?php echo $post->getNotificationTitle()?>*<br>
 
 <?php echo \Idno\Core\Idno::site()->language()->_('View post:') ?> <?php echo $post->getDisplayURL()?>
+<?php echo "\n\n" . \Idno\Core\Idno::site()->language()->getLanguage(); ?>
 <?php
     unset($this->vars['notification']);
--- templates/email/content/notification/like.tpl.php
+++ templates/email/content/notification/like.tpl.php
@@ -3,8 +3,9 @@ $notification = $vars['notification'];
 $annotation   = $notification->getObject();
 $post         = $notification->getTarget();
 ?>
-<a href="<?php echo $annotation['owner_url']?>"><img src="<?php echo $annotation['owner_image']?>" style="width: 100px; margin-left: 10px; margin-bottom: 10px" align="right"></a>Hi! We wanted to let you know that <strong><a href="<?php echo $annotation['owner_url']?>"><?php echo $annotation['owner_name']?></a></strong>
-liked the post <strong><a href="<?php echo $post->getDisplayURL();?>"><?php echo $post->getNotificationTitle()?></a></strong><br>
+<a href="<?php echo $annotation['owner_url']?>"><img src="<?php echo $annotation['owner_image']?>" style="width: 100px; margin-left: 10px; margin-bottom: 10px" align="right"></a><?php 
+  echo \Idno\Core\Idno::site()->language()->_('Hi! We wanted to let you know that <strong>%s</strong> liked the post <strong>%s</strong>',
+['<a href="' . $annotation['owner_url'] . '">' . $annotation['owner_name'] . '</a>', '<a href="' . $post->getDisplayURL() . '">' . $post->getNotificationTitle() . '</a>']) ?><br>
 <br class="clearall">
 <div class="center">
     <a href="<?php echo $post->getDisplayURL()?>" style="background-color:#73B2E3;border:1px solid #73B2E3;border-radius:4px;color:#ffffff;display:inline-block;font-family:sans-serif;font-size:17px;font-weight:normal;line-height:40px;text-align:center;text-decoration:none;width:200px;-webkit-text-size-adjust:none;mso-hide:all;"><?php echo \Idno\Core\Idno::site()->language()->_('View post') ?></a>
